74955671c29026770269811ee2a3274fe2cdeaf6
kumkumhirve <kumkumhirve452@gmail.com>
2025-05-11 22:38:07 +0530

    Add files via upload

---
+++ add_element.py
@@ -0,0 +1,14 @@
+'''Q3: WAP to add all elements of list containing first 20 natural
+number?'''
+
+# Create a list of the first 20 natural numbers (1 to 20)
+natural_numbers = list(range(1, 21))
+
+# Calculate the sum of all elements in the list
+total = sum(natural_numbers)
+
+# Display the result
+print("The list of first 20 natural numbers is:")
+print(natural_numbers)
+
+print("\nThe sum of all elements is:", total)
